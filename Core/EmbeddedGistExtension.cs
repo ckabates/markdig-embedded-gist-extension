e3c2cce67b9dc529d0735db632abe44276b4b5f2
Colin Bates <ckabates@gmail.com>
2020-08-16 11:04:49 +1000

    Add basic classes

---
+++ Core/EmbeddedGistExtension.cs
@@ -0,0 +1,21 @@
+﻿using Markdig;
+using Markdig.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkdigEmbeddedGistExtension.Core
+{
+    public class EmbeddedGistExtension : IMarkdownExtension
+    {
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
