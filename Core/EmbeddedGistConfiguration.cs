fd67eead0d99225f318993586bdd67aefd5c6325
Colin Bates <ckabates@gmail.com>
2020-08-16 16:40:20 +1000

    Develop Match method on EmbeddedGistInlineParser and allow use of EmbeddedGistConfiguration

---
+++ Core/EmbeddedGistConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkdigEmbeddedGistExtension.Core
+{
+    public class EmbeddedGistConfiguration
+    {
+        public List<string> BaseUrls { get; set; } = new List<string> { "https://gist.github.com" };
+
+        public bool IsMockRender => MockRenderFragment != string.Empty;
+
+        public string MockRenderFragment { get; set; } = string.Empty;
+
+        public void UseMockRender(string renderFragment)
+        {
+            MockRenderFragment = renderFragment;
+        }
+
+        public void AddBaseUrl(string url)
+        {
+            if (!BaseUrls.Contains(url))
+            {
+                BaseUrls.Add(url);
+            }
+        }
+    }
+}
--- Core/EmbeddedGistExtensionMethods.cs
+++ Core/EmbeddedGistExtensionMethods.cs
@@ -1,4 +1,5 @@
 ﻿using Markdig;
+using System;
 
 namespace MarkdigEmbeddedGistExtension.Core
 {
@@ -9,6 +10,19 @@ namespace MarkdigEmbeddedGistExtension.Core
             if (!pipeline.Extensions.Contains<EmbeddedGistExtension>())
             {
                 pipeline.Extensions.Add(new EmbeddedGistExtension());
+            }
+            return pipeline;
+        }
+
+        public static MarkdownPipelineBuilder UseEmbeddedGists(
+            this MarkdownPipelineBuilder pipeline, 
+            Action<EmbeddedGistConfiguration> configuration)
+        {
+            if (!pipeline.Extensions.Contains<EmbeddedGistExtension>())
+            {
+                var _options = new EmbeddedGistConfiguration();
+                configuration(_options);
+                pipeline.Extensions.Add(new EmbeddedGistExtension(_options));
             }
             return pipeline;
         }
